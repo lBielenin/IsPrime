78dd209e303ad21c0076d0a01ac0a6c3a586bc31
Łukasz Bielenin <Lukasz.Bielenin@microsoft.wsei.edu.pl>
2021-10-21 19:00:03 +0200

    Add solution

---
+++ IsPrime/PrimeGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace IsPrime
+{
+    internal class PrimeGenerator
+    {
+        public List<int> GeneratePrimes(int numberOfPrimes)
+        {
+            List<int> primeNumbers = new() { 2 };
+            int currentNumber = 3;
+            while (primeNumbers.Count < numberOfPrimes)
+            {
+                double sqrt = Math.Sqrt(currentNumber);
+
+                if(isWhole(sqrt))
+                {
+                    bool isPrime = true;
+
+                    foreach(var prime in primeNumbers.Where(num => num <= (int)sqrt))
+                    {
+                        if (currentNumber % prime == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                    if (isPrime)
+                    {
+                        primeNumbers.Add(currentNumber);
+                    }
+                }
+
+                currentNumber += 2;
+            }
+            return primeNumbers;
+        }
+
+        private bool isWhole(double num) => num % 1 == 0;
+    }
+}
---
+++ IsPrime/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using static System.Console;
+
+namespace IsPrime
+{
+    class Program
+    {
+        private static readonly PrimeGenerator _primeGenerator = new PrimeGenerator();
+
+        static void Main(string[] args)
+        {
+            WriteLine("Welcome to Prime Generator");
+
+            var numberOfPrimes = GetUserInput();
+
+            WriteLine($"The first {numberOfPrimes} prime numbers are:");
+            _primeGenerator.GeneratePrimes(numberOfPrimes).ForEach(num => WriteLine(num));
+
+            WriteLine($"Press any key to exit application");
+
+            ReadKey();
+
+        }
+
+        private static int GetUserInput()
+        {
+            int result = 0;
+            var isValid = false;
+
+            WriteLine("Provide valid number of primes");
+
+            do
+            {
+                var input = ReadLine();
+
+                if (int.TryParse(input, out int number))
+                {
+                    result = number;
+                    isValid = true;
+                }
+                else
+                {
+                    WriteLine("Invalid input!");
+                }
+            }
+            while (!isValid);
+
+            return result;
+        }
+    }
+}
